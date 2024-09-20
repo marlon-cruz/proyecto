aa2ac40d8b3fc289d0dff35c53131dbe43e7e19b
Marlon <marlo@MARLON>
2024-09-19 18:38:09 -0600

    Prueba

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,19 +34,21 @@ namespace proyecto1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(235, 155);
+            this.button1.Location = new System.Drawing.Point(28, 29);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(282, 23);
+            this.button1.Size = new System.Drawing.Size(376, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "QUE FUNCIONE PORFA";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
